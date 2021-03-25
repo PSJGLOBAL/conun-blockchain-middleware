caea3e955a6cf19c7c36f1e4af8a6df4e0da3e40
Ruffiano <ruffianodev@gmail.com>
2021-03-25 19:07:31 +0900

    con transaction

--- routes/ether/eth.js
+++ routes/ether/eth.js
@@ -40,6 +40,41 @@ router.post('/SendETH', async (req, res) => {
     })
 })
 
+router.post('/SendCON', async (req, res) => {
+    helper.getUserIdentity({
+        orgName: req.body.orgName,
+        walletAddress: req.body.fromAddress,
+        walletType: req.body.walletType,
+        password: req.body.password
+    }).then((ledgerData) => {
+        Eth.keyStoreDecrypt(ledgerData.keyStore, req.body.password).then((result) => {
+            Eth.SendCON({
+                fromAddress: req.body.fromAddress,
+                toAddress: req.body.toAddress,
+                type: req.body.type,
+                value: req.body.value,
+                gasLimit: req.body.gasLimit,
+                gasPrice: req.body.gasPrice,
+                privateKey: result.privateKey
+            }).then((txHash) => {
+                console.log('balance: ', txHash)
+                res.status(200).json({
+                    payload: txHash,
+                    success: true,
+                    status: 200
+                });
+            }).catch((error) => {
+                res.status(400).json({
+                    payload: error,
+                    success: false,
+                    status: 400
+                });
+            })
+        })
+    })
+})
+
+
 router.get('/getBalanceOfEth', async (req, res) => {
     Eth.getBalanceOfEth(req.query.walletAddress)
         .then((balance) => {
